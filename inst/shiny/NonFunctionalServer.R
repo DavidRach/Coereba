3739cbcda122ac4d896791e69d14735e88d6ce3c
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-04-07 15:47:46 -0400

    Java is now seeing the title correctly on click capture.

--- inst/shiny/NonFunctionalServer.R
+++ inst/shiny/NonFunctionalServer.R
@@ -1,5 +1,5 @@
 server <- function(input, output, session) {
-  click_info <- reactiveValues(click_data = data.frame(Plot_Name = character(), X_Coordinate = numeric()))
+  #click_info <- reactiveValues(click_data = data.frame(Plot_Name = character(), X_Coordinate = numeric()))
 
   # Read the uploaded CSV file
   data <- reactive({
@@ -82,8 +82,10 @@ server <- function(input, output, session) {
             "
             function(el, x) {
               el.on('plotly_click', function(data) {
+                console.log('Click event captured');
+                console.log(data);
                 Shiny.setInputValue('plot_click', {
-                  plot_name: data.points[0].data.name,
+                  plot_name: x.layout.title.text,
                   x_coordinate: data.points[0].x
                 });
               });
@@ -102,14 +104,18 @@ server <- function(input, output, session) {
   # Observe the click event and store information
   observeEvent(input$plot_click, {
     print("Click event captured!")
-    if(nrow(click_info$click_data) == 0) {
-      click_info$click_data <- data.frame(Plot_Name = input$plot_click$plot_name,
-                                          X_Coordinate = input$plot_click$x_coordinate)
-    } else {
-      click_info$click_data <- rbind(click_info$click_data,
-                                     data.frame(Plot_Name = input$plot_click$plot_name,
-                                                X_Coordinate = input$plot_click$x_coordinate))
-    }
+
+    print(paste("Plot Name:", input$plot_click$plot_name))
+    print(paste("X Coordinate:", input$plot_click$x_coordinate))
+
+    #if(nrow(click_info$click_data) == 0) {
+    #  click_info$click_data <- data.frame(Plot_Name = input$plot_click$plot_name,
+    #                                      X_Coordinate = input$plot_click$x_coordinate)
+    #} else {
+    #  click_info$click_data <- rbind(click_info$click_data,
+    #                                 data.frame(Plot_Name = input$plot_click$plot_name,
+    #                                            X_Coordinate = input$plot_click$x_coordinate))
+    #}
   })
 
   # Render the clickDataTable with the updated click data
