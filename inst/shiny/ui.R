650139229e59efefd4a658dca08c1ece51a4244e
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-04-07 21:08:37 -0400

    Bumped tonight's version of the shiny app ui and server to the CoerebaApp side. Modifications can continue under the non-functional pages, and be archived in the main as needed.

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,23 +1,78 @@
-ui <- fluidPage(
-  titlePanel("GatingSet Explorer"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput("gatingSetName", "Enter GatingSet Object Name", value = ""),
-      textInput("x", "Enter x value", value = "Comp-BUV737-A"),
-      textInput("y", "Enter y value", value = "Comp-Spark Blue 550-A"),
-      textInput("sampleName", "Enter Sample Name", value = "GROUPNAME"),
-      numericInput("bins", "Enter bins", value = 70),
-      numericInput("clearance", "Enter clearance", value = 0.2),
-      textInput("removeStrings", "Enter remove strings", value = ""),
-      textInput("marginSubset", "Enter margin subset", value = "Tcells"),
-      textInput("gateSubset", "Enter gate subset", value = "Vd2"),
-      checkboxInput("gateLines", "Gate lines", value = TRUE),
-      checkboxInput("pdfOutput", "Generate PDF Output", value = TRUE),
-      textInput("outputPath", "Enter Output Path", value = ""),
-      actionButton("generatePlots", "Generate Plots")
-    ),
-    mainPanel(
-      uiOutput("plots")
+ui <- dashboardPage(
+  dashboardHeader(title = "Coereba"),
+
+  ## Sidebar
+  dashboardSidebar(
+    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")),
+    tags$script(src = "tips.js"),
+    sidebarMenu(
+      id = "menu",
+      menuItem("Upload CSV File", tabName = "data", icon = icon("table")),
+      menuItem("Upload a GatingSet", tabName = "gatingset", icon = icon("table")),
+      menuItem("Click Data", tabName = "clickdata", icon = icon("table"))
+    )
+  ),
+
+  ## Body
+  dashboardBody(
+    shinyjs::useShinyjs(),
+    tabItems(
+      tabItem(
+        tabName = "data",
+        titlePanel("Upload CSV File"),
+        sidebarLayout(
+          sidebarPanel(
+            width = 3,
+            fileInput("file", "Choose CSV File")
+          ),
+          mainPanel(
+            div(
+              style = "max-height: 400px; overflow-y: auto;",
+              tableOutput("contents")
+            )
+          )
+        )
+      ),
+      tabItem(
+        tabName = "gatingset",
+        titlePanel("GatingSet Explorer"),
+        sidebarLayout(
+          sidebarPanel(
+            width = 3,
+            textInput("gatingSetName", "Enter GatingSet Object Name", value = "gs"),
+            selectInput("x_variable", "Select x-axis variable", choices = NULL),
+            selectInput("y_variable", "Select y-axis variable", choices = NULL),
+            textInput("sampleName", "Enter Sample Name Keyword", value = "GROUPNAME"),
+            numericInput("bins", "Enter desired bins", value = 70),
+            numericInput("clearance", "Enter clearance multiplier", value = 0.2),
+            textInput("removeStrings", "Enter string values to remove from the name", value = "c"),
+            textInput("marginSubset", "Enter desired margin subset", value = "Tcells"),
+            textInput("gateSubset", "Enter desired gate subset", value = "Vd2"),
+            checkboxInput("gateLines", "Display Estimated Gate Cutoffs", value = TRUE),
+            actionButton("generatePlots", "Generate Plots")
+          ),
+          mainPanel(
+            width = 9,
+            uiOutput("plots")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "clickdata",
+        titlePanel("Click Data"),
+        fluidRow(
+          column(
+            width = 6,
+            textInput("export_filename", "Export Filename", value = "click_data")
+          ),
+          column(
+            width = 6,
+            textInput("export_directory", "Export Directory Path", placeholder = "/path/to/directory")
+          )
+        ),
+        actionButton("export_button", "Export Click Data", icon = icon("download")),
+        DTOutput("clickDataTable")
+      )
     )
   )
 )
