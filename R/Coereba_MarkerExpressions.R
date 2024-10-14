43637621af7a634bc76ada6a2e5a40150dc13e51
David Rach <drach@som.umaryland.edu>
2024-10-14 16:11:53 -0400

    Breaking changes to what was Utility_MarkerExpressions.
    Coereba_MarkerExpressions holds the aggregate capacity, and now can return "All", or "Combinatorial" which estimates proportion for Q1->Q4.
    
    The plotting functionality has been broken off into Utility_MarkerPlots that appears to be able to handle both returnType style outputs.

---
+++ R/Coereba_MarkerExpressions.R
@@ -0,0 +1,194 @@
+#' Generate overall marker expressions across Coereba clusters,
+#' 
+#' @param data The data.frame of clusters vs individual specimens, with the ratio values
+#' @param binary The data.frame of markers vs clusters, with 0 and 1 values
+#' @param panel A .csv or data.frame containing Fluorophore and Marker columns of your panel markers.
+#' @param starter A string containing the starting characters for all Coereba cluster names
+#' @param returnType Either "All" or "Combinatorial". Default is ALL. 
+#' @param CombinatorialArgs When returnType Combinatorial, the two fluorophores to create quadrants for.
+#'
+#' @importFrom utils read.csv
+#' @importFrom dplyr select
+#' @importFrom tidyselect starts_with
+#' @importFrom purrr map
+#' @importFrom dplyr bind_cols
+#' @importFrom dplyr rename
+#' @importFrom dplyr left_join
+#'
+#' @return returns a data.frame
+#' @export
+#'
+#' @examples NULL
+
+Coereba_MarkerExpressions <- function(data, binary, panel, starter, returnType="All",
+CombinatorialArgs=NULL){
+
+  if (!is.data.frame(panel)) {MyPanel <- read.csv(panel, check.names = FALSE)
+  } else {MyPanel <- panel}
+
+  Metadata <- data %>% select(!starts_with(starter))
+
+  AllMarkers <- binary %>% select(!starts_with("Identity")) %>% colnames()
+  #x <- AllMarkers[1]
+  
+  if (returnType == "Combinatorial" && !is.null(CombinatorialArgs)){
+    #CombinatorialArgs <- c("BV510", "APC-Fire 750")
+    internalstrings <- c(" ", "-", "_", ".")
+    TheCombinatorialArgs <- NameCleanUp(CombinatorialArgs, removestrings=internalstrings)
+    if (length(TheCombinatorialArgs) != 2){
+      stop("Combinatorial Args should be a list of two fluorophores to generate quadrants from")
+    }
+
+    SwampPuppy <-CombinatorialAggregate(x=TheCombinatorialArgs, data=data, binary=binary, panel=MyPanel) 
+  }
+
+  if (returnType == "All"){
+  # Return Marker Expressions for All Markers
+  SwampPuppy <- map(.x=AllMarkers, .f=.Internal_Aggregate, data=data, 
+    binary=binary) %>% bind_cols()
+  
+  # Swap out Fluorophore for Marker Names
+  SwampFluors <- SwampPuppy %>% colnames()
+  SwampFluors <- data.frame(SwampFluors) %>% rename(Fluorophore = SwampFluors)
+  RetainedFluors <- left_join(SwampFluors, MyPanel, by = "Fluorophore")
+  NewNames <- RetainedFluors$Marker
+  colnames(SwampPuppy) <- NewNames
+  }
+
+  #Reattach Metadata to Summarized Marker Expressions
+  MarkerExpressions <- cbind(Metadata, SwampPuppy)
+
+  return(MarkerExpressions)
+  }
+
+#' Internal for Coereba_MarkerExpressions
+#'
+#' @param x The two fluorophores to generate quadrants from
+#' @param data The data.frame of clusters vs individual specimens, with the ratio values
+#' @param binary The data.frame of markers vs clusters, with 0 and 1 values
+#' @param panel The passed Panel data.frame
+#'
+#' @importFrom dplyr rename
+#' @importFrom dplyr filter
+#' @importFrom dplyr left_join
+#'
+#' @noRd
+CombinatorialAggregate <- function(x, data, binary, panel){
+  First <- x[1]
+  Second <- x[2]
+
+  SwampFluors <- rbind(First, Second)
+  SwampFluors <- data.frame(SwampFluors) %>% rename(Fluorophore = SwampFluors)
+  RetainedFluors <- left_join(SwampFluors, MyPanel, by = "Fluorophore")
+  NewNames <- RetainedFluors$Marker
+  
+  Q1 <- binary %>% dplyr::filter(.data[[First]] == 0 & .data[[Second]] == 1)
+  Q1_Label <- paste0(NewNames[1], "-", NewNames[2], "+")
+  Q1_Assembly <- DataRetrieval(x=Q1, data=data, binary=binary, Column=Q1_Label)
+
+  Q2 <- binary %>% dplyr::filter(.data[[First]] == 1 & .data[[Second]] == 1)
+  Q2_Label <- paste0(NewNames[1], "+", NewNames[2], "+")
+  Q2_Assembly <- DataRetrieval(x=Q2, data=data, binary=binary, Column=Q2_Label)
+
+  Q3 <- binary %>% dplyr::filter(.data[[First]] == 1 & .data[[Second]] == 0)
+  Q3_Label <- paste0(NewNames[1], "+", NewNames[2], "-")
+  Q3_Assembly <- DataRetrieval(x=Q3, data=data, binary=binary, Column=Q3_Label)
+
+  Q4 <- binary %>% dplyr::filter(.data[[First]] == 0 & .data[[Second]] == 0)
+  Q4_Label <- paste0(NewNames[1], "-", NewNames[2], "-")
+  Q4_Assembly <- DataRetrieval(x=Q4, data=data, binary=binary, Column=Q4_Label)
+
+  SwampPuppy <- cbind(Q1_Assembly, Q2_Assembly, Q3_Assembly, Q4_Assembly)
+}
+
+#' Internal for CombinatorialAggregate Coereba_MarkerExpressions
+#' 
+#' @param x The two fluorophores to quadrant by
+#' @param data The data.frame of clusters vs individual specimens, with the ratio values
+#' @param binary The data.frame of markers vs clusters, with 0 and 1 values
+#' @param Column The label name for the respective quadrant
+#' 
+#' @importFrom tidyr as_tibble
+#' @importFrom dplyr rowwise
+#' @importFrom dplyr mutate
+#' @importFrom dplyr c_across
+#' @importFrom tidyselect everything 
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate_all
+#' 
+DataRetrieval <- function(x, data, binary, Column){
+  Positive <- x
+    #Retrieve corresponding Clusters from data
+    TheInternalBypass <- Positive$Identity
+    TheInternalBypass <- gsub("_", "", TheInternalBypass)
+    TheInternalBypass <- gsub("-", "", TheInternalBypass)
+    InternalData <- data[, names(data) %in% TheInternalBypass] 
+    InternalData <- as_tibble(InternalData)
+  
+    if(nrow(Positive) >0){
+      #Aggregate the Ratio Values
+      Subsetted <- InternalData %>% rowwise() %>% mutate(
+        aggregate = sum(c_across(everything()), na.rm = TRUE))
+      InternalFinal <- Subsetted %>% select(aggregate)
+  
+    } else {
+      #Aggregate the Ratio Values  
+      Subsetted <- InternalData %>% rowwise() %>% mutate(
+        aggregate = sum(c_across(everything()), na.rm = TRUE))
+      InternalFinal <- Subsetted %>% select(aggregate) %>%
+        mutate_all(~0) # Since No Positive Columns
+    }
+
+    colnames(InternalFinal)[1] <- Column
+  return(InternalFinal)
+}
+
+#' Internal for Coereba_MarkerExpressions
+#'
+#' @param x Iterated Marker
+#' @param data The data.frame of clusters vs individual specimens, with the ratio values
+#' @param binary The data.frame of markers vs clusters, with 0 and 1 values
+#'
+#' @importFrom dplyr filter
+#' @importFrom tidyr as_tibble 
+#' @importFrom dplyr rowwise
+#' @importFrom dplyr mutate
+#' @importFrom dplyr c_across
+#' @importFrom tidyselect everything 
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate_all
+#'
+#' @noRd
+.Internal_Aggregate <- function(x, data, binary){
+  Column <- x
+
+  # Retrieve Clusters that are Positive for Iterated Marker
+  Positive <- binary %>% dplyr::filter(.data[[Column]] == 1)
+
+  #Retrieve corresponding Clusters from data
+  TheInternalBypass <- Positive$Identity
+  TheInternalBypass <- gsub("_", "", TheInternalBypass)
+  TheInternalBypass <- gsub("-", "", TheInternalBypass)
+  InternalData <- data[, names(data) %in% TheInternalBypass] 
+  InternalData <- as_tibble(InternalData)
+
+  if(nrow(Positive) >0){
+    #Aggregate the Ratio Values
+    Subsetted <- InternalData %>% rowwise() %>% mutate(
+      aggregate = sum(c_across(everything()), na.rm = TRUE))
+    InternalFinal <- Subsetted %>% select(aggregate)
+
+  } else {
+    #Aggregate the Ratio Values  
+    Subsetted <- InternalData %>% rowwise() %>% mutate(
+      aggregate = sum(c_across(everything()), na.rm = TRUE))
+    InternalFinal <- Subsetted %>% select(aggregate) %>%
+      mutate_all(~0) # Since No Positive Columns
+  }
+
+  #Change Name  
+  colnames(InternalFinal)[1] <- Column
+
+  return(InternalFinal)
+}
+
---
+++ R/Utility_MarkerPlots.R
@@ -0,0 +1,91 @@
+#' Takes Coereba_MarkerExpressions data.frame and returns Marker Expression Beeswarm plots
+#' 
+#' @param data A data.frame of metadata and summarized markers from Utility_MarkerExpressions
+#' @param panel A .csv or data.frame containing Fluorophore and Marker columns of your panel markers.
+#' @param myfactor The column name by which you want to group outputs in the plot
+#' @param shape_palette A scale manual list assigning shape to factor level
+#' @param fill_palette A scale fill list assigning fill to factor level
+#' @param cex geom_beeswarm argument, default 1
+#' @param size geom_beeswarm argument, default 1.5
+#' @param corral.width geom_beeswarm argument, default 1
+#' @param crossbar geom_boxplot argument, default "median"
+#' @param XAxisLevels Provide list marker names correct order for x-axis reordering, default NULL
+#' @param savePlot Whether to save ggplot object to outfolder, default = FALSE
+#' @param outpath Specify file.path to desired storage location
+#' @param filename Specify desired filename
+#' @param dpi Specify desired pixel resolution
+#' @param width Specify desired width inches
+#' @param height Specify desired height inches
+#' @param filterForThese A list containing names of markers desire to include final plot, default NULL
+#'  
+#' @importFrom utils read.csv
+#' @importFrom dplyr pull
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyselect all_of
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_boxplot
+#' @importFrom ggplot2 stat_summary
+#' @importFrom ggbeeswarm geom_beeswarm
+#' @importFrom ggplot2 scale_shape_manual
+#' @importFrom ggplot2 scale_fill_manual
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 element_blank()
+#' @importFrom ggplot2 element_text()
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 ggsave
+#' @importFrom dplyr filter
+#' 
+#' @return Returns a ggplot object to R or designated Folder
+#' @export
+#' 
+#' @examples NULL
+Utility_MarkerPlots <- function(data, panel, myfactor, shape_palette,
+   fill_palette, cex=1, size=1.5, corral.width=1, crossbar="median", XAxisLevels=NULL,
+   savePlot=FALSE, outpath=NULL, filename = NULL, dpi = 600, width=9, height=3,
+   filterForThese=NULL, combinatorialStartsWith=NULL){
+  
+  if (!is.data.frame(panel)) {MyPanel <- read.csv(panel, check.names = FALSE)
+  } else {MyPanel <- panel}
+
+  TheColumnNames <- colnames(data)
+
+  if(is.null(combinatorialStartsWith)) {DataColNames <- MyPanel %>% pull(Marker)
+  } else {DataColNames <- data %>% select(starts_with(combinatorialStartsWith)) %>% colnames()}
+  
+  MetaColumns <- setdiff(TheColumnNames, DataColNames)
+
+  MeltedData <- data %>% pivot_longer(
+    cols = -all_of(MetaColumns),
+    names_to = "Marker",
+    values_to = "Value"
+  )
+
+  if(!is.null(filterForThese))(MeltedData <- MeltedData %>% dplyr::filter(Marker %in% filterForThese))
+
+  if(!is.null(XAxisLevels)){
+    MeltedData$Marker <- factor(MeltedData$Marker, levels = XAxisLevels)}
+
+  ThePlot <- ggplot(MeltedData, aes(x = Marker, y = Value)) +
+    geom_boxplot(show.legend = FALSE) + stat_summary(fun = crossbar,
+       show.legend = FALSE, geom = "crossbar", width = 0.75) + 
+    geom_beeswarm(show.legend = FALSE, aes(shape = .data[[myfactor]],
+       fill = .data[[myfactor]]), method = "center", side = 0, priority = "density",
+        cex = cex, size = size, corral = "wrap", corral.width = corral.width) +
+    scale_shape_manual(values = shape_palette) + 
+    scale_fill_manual(values = fill_palette) +
+    labs(title = NULL, x = NULL, y = NULL) +
+    theme_bw() + theme(panel.grid.major = element_blank(), 
+                        panel.grid.minor = element_blank(), 
+                        plot.title = element_text(hjust = 0.5, size = 8))
+
+  if (savePlot == TRUE){
+    if(is.null(outpath)){StorageLocation <- getwd()
+    } else {StorageLocation <- outpath}
+    TheFileName <- paste0(filename, ".png")
+    SendMeOnMyWay <- file.path(StorageLocation, TheFileName)
+    ggsave(SendMeOnMyWay, ThePlot, dpi = dpi, units = "in",
+     width = width, height = height)
+    } else {return(ThePlot)}
+}
