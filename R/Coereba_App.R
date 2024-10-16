9a3e236d563c70a6d43a5010e47c1e94c79a5a42
David Rach <drach@som.umaryland.edu>
2024-10-16 13:34:20 -0400

    Coereba_App done

--- R/Coereba_App.R
+++ R/Coereba_App.R
@@ -1,10 +1,37 @@
-#' Deploy a Shiny app to modify the estimated gate cutoffs.
+#' Deploy a Shiny app to modify the estimated gate cutoff placement.
 #'
 #' @importFrom shiny runApp
 #'
 #' @export
-
-
+#' 
+#' @examples
+#' 
+#' library(flowCore)
+#' library(flowWorkspace)
+#' library(openCyto)
+#' library(data.table)
+#' 
+#' File_Location <- system.file("extdata", package = "Coereba")
+#' TheCSV <- file.path(File_Location, "GateCutoffsForNKs.csv")
+#' 
+#' FCS_Files <- list.files(path = File_Location, pattern = ".fcs", full.names = TRUE)
+#' UnmixedFCSFiles <- FCS_Files[1]
+#' UnmixedCytoSet <- load_cytoset_from_fcs(UnmixedFCSFiles,
+#'  truncate_max_range = FALSE, transformation = FALSE)
+#' UnmixedGatingSet <- GatingSet(UnmixedCytoSet)
+#' Markers <- colnames(UnmixedCytoSet)
+#' KeptMarkers <- Markers[-grep("Time|FS|SC|SS|Original|-W$|-H$|AF", Markers)]
+#' biex_transform <- flowjo_biexp_trans(channelRange = 256, maxValue = 1000000,
+#'  pos = 4.5, neg = 0, widthBasis = -1000)
+#' TransformList <- transformerList(KeptMarkers, biex_transform)
+#' flowWorkspace::transform(UnmixedGatingSet, TransformList)
+#' UnmixedGates <- fread(file.path(path = File_Location,
+#'  pattern = 'GatesUnmixed.csv'))
+#' UnmixedGating <- gatingTemplate(UnmixedGates)
+#' gt_gating(UnmixedGating, UnmixedGatingSet)
+#' 
+#' # Coereba_App()
+#' 
 Coereba_App <- function() {
     runApp(
     system.file("shiny", package="Coereba"),
--- tests/testthat/test-Coereba_App.R
+++ tests/testthat/test-Coereba_App.R
@@ -6,7 +6,8 @@ TheCSV <- file.path(File_Location, "GateCutoffsForNKs.csv")
 ShinyLocation <- system.file("shiny", package = "Coereba")
 
 test_that("{shinytest2} recording: CVSLocate", {
-  app <- AppDriver$new(app_dir = ShinyLocation, name = "CVSLocate", height = 959, width = 1619)
+  app <- AppDriver$new(app_dir = ShinyLocation, name = "CVSLocate",
+    height = 959, width = 1619)
   app$upload_file(file = TheCSV)
   app$set_inputs(menu = "gatingset")
   app$expect_values()
