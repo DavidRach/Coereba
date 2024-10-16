09cd6e78211ce6909b394ca8564a2b2d67c8323a
David Rach <drach@som.umaryland.edu>
2024-10-16 12:02:42 -0400

    One working shinytest2 for CoerebaApp

---
+++ tests/testthat/test-ShinyCSVLoad.R
@@ -0,0 +1,13 @@
+library(testthat)
+library(shinytest2)
+
+File_Location <- system.file("extdata", package = "Coereba")
+TheCSV <- file.path(File_Location, "GateCutoffsForNKs.csv")
+ShinyLocation <- system.file("shiny", package = "Coereba")
+
+test_that("{shinytest2} recording: CVSLocate", {
+  app <- AppDriver$new(app_dir = ShinyLocation, name = "CVSLocate", height = 959, width = 1619)
+  app$upload_file(file = TheCSV)
+  app$set_inputs(menu = "gatingset")
+  app$expect_values()
+})
