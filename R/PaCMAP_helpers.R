de61f0b3186e92c7c5aa6ff5486b1e577787eccc
David Rach <drach@som.umaryland.edu>
2025-02-22 09:10:44 -0500

    With Utility_Coereba now having integrated dictionary, continuing generalization of the PaCMAP_helpers functions to write the Coereba_Processing wrapper to assemble the data into an S4 summarized experiment object. Up to the "C" data.frame creation step currently.

--- R/PaCMAP_helpers.R
+++ R/PaCMAP_helpers.R
@@ -1,73 +1,110 @@
-#' Takes Utility_Coereba output and returns a Bioconductor Summarized Experiment object
+#' Takes Utility_Coereba output, plus additional metadata,and returns as a
+#'  Bioconductor Summarized Experiment object
+#' 
+#' @param x An object with Coereba gating and metadata. Acccepted arguments
+#'  include  a file.path to an .fcs, a flowFrame or cytoFrame, and a data.frame. 
+#' @param metadata A data.frame or file.path to a metadata.csv file
+#' @param metadata_columns A vector of column names from the metadata want to incorporate.
+#' 
+#' @importFrom flowWorkspace load_cytoframe_from_fcs
+#' @importFrom dplyr select
+#' @importFrom tidyselect all_of
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarise
+#' @importFrom dplyr n
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom tidyr pivot_wider
+#' @importFrom utils read.csv
+#' 
+#' 
+#' 
+#' 
+#'
+#' 
+#' @return A summarized experiment object
 #' 
 #' @noRd
-Coereba_Processing <- function(x, TheDictionary=NULL,  Sprint="%03d", ReplaceCharacters=NULL,
-Metadata, Identity, thecolumns){
+Coereba_Processing <- function(x, metadata=NULL, metadata_columns=NULL, 
+  
+Identity){
 
-  if(!is.data.frame(x)){
-    stop("Please provide Utility_Coereba output consisting of a data.frame 
-    with the exprs and Coereba columns")
-  }
+  if (class(x) == "character"){
+    Coereba <- load_cytoframe_from_fcs(x, truncate_max_range = FALSE, 
+    transformation = FALSE)
+  } else if (class(x) == "flowFrame"){Coereba <- x
+  } else if (class(x) == "cytoframe"){Coereba <- x
+  } else if (is.data.frame(x)){Coereba <- x
+  } else {message("Argument x is a class ", class(x), " object, which is not recognized")}
 
-  TheIsolatedData <- data.frame(x, check.names = FALSE)
+  if (class(Coereba) %in% c("flowFrame", "cytoframe")){
+    CoerebaOne <- Coereba_FCS_Reversal(Coereba=Coereba)
+  } else if (is.data.frame(Coereba)){KnownColumns <- c("Cluster", "specimen")
+    CoerebaOne <- Coereba |> select(all_of(KnownColumns))
+  } else {message("Class not recognized at reversal step")}
 
-  if(!is.null(TheDictionary)){
+    # Derriving Count and Ratio
+    TheClusters <- CoerebaOne |> group_by(specimen, Cluster) |>
+      summarise(Count = n()) |> ungroup() #Counts clusters per specimen
+    TheSpecimens <- CoerebaOne |> group_by(specimen) |>
+      summarise(SpecimenCount = n()) |> ungroup() #Counts per Specimen
+    Merging <- left_join(TheClusters, TheSpecimens, by = "specimen")
+    Data <- Merging |> mutate(Ratio = Count / SpecimenCount)
+  
+    Ratio <- Data |> select(-Count, -SpecimenCount) |>
+      pivot_wider(names_from = specimen, values_from = Ratio)
+    Ratio[is.na(Ratio)] <- 0
 
-    if(!is.data.frame(TheDictionary)){
-      Dictionary <- read.csv(TheDictionary, check.names=FALSE)
-    } else {Dictionary <- TheDictionary}
-
-    Conjoined <- left_join(TheIsolatedData, Dictionary, by="ClusterNumber")
-    Conjoined <- Conjoined %>% select(-all_of(c("Freq", "ClusterNumber")))
-
-    TheIsolatedData <- Conjoined
-  }
-
-  if(!is.null(ReplaceCharacters)){
-
-    if (!is.null(Sprint)){
-      TheIsolatedData$specimen <- sprintf(Sprint, TheIsolatedData$specimen)
-    }
-    
-    TheIsolatedData$specimen <- paste0(ReplaceCharacters, TheIsolatedData$specimen)
-  }
-
-  TheIsolatedData <- TheIsolatedData %>% select(specimen, Cluster)
-  #TheIsolatedData <- TheIsolatedData %>% select(.data[[specimen]], Cluster)
-  #colnames(TheIsolatedData)[[1]] <- "specimen"
-
-  # Derriving Count and Ratio
-  TheClusters <- TheIsolatedData %>% group_by(specimen, Cluster) %>%
-    summarise(Count = n()) %>% ungroup() #Counts clusters per specimen
-  TheSpecimens <- TheIsolatedData %>% group_by(specimen) %>%
-    summarise(SpecimenCount = n()) %>% ungroup() #Counts per Specimen
-  Merging <- left_join(TheClusters, TheSpecimens, by = "specimen")
-  Data <- Merging %>% mutate(Ratio = Count / SpecimenCount)
-
-  Ratio <- Data %>% select(-Count, -SpecimenCount) %>%
-    pivot_wider(names_from = specimen, values_from = Ratio)
-  Ratio[is.na(Ratio)] <- 0
-
-  # Derriving Assay Objects for S4
-  Counts <- Data %>% select(-Ratio, -SpecimenCount) %>% 
-    pivot_wider(names_from = specimen, values_from = Ratio)
-  Counts[is.na(Counts)] <- 0
-
+    Counts <- Data |> dplyr::select(-"Ratio", -"SpecimenCount") |>
+      pivot_wider(names_from = specimen, values_from = "Count")
+    Counts[is.na(Counts)] <- 0
+  
   # Assembling Metadata
+  if (is.null(metadata)){
+    Names <- colnames(Counts)
+    Names <- Names[-grep("Cluster", Names)]
+    Metadata <- data.frame(specimen=Names)
+  } else {
+    if (!is.data.frame(metadata)){
+      Metadata <- read.csv(metadata, check.names = FALSE)
+    } else {Metadata <- metadata}
+  
+    if (!is.null(thecolumns)){
+      Metadata <- Metadata |> select(all_of(metadata_columns))
+    }
 
-  if(!is.data.frame(Metadata)){Metadata <- read.csv(Metadata, check.names = FALSE)
-  } else {Metadata <- Metadata}
-
-  if (!is.null(thecolumns)){
-    Metadata <- Metadata %>% select(all_of(thecolumns))
+  #Metadata <- Metadata %>% rename(specimen = all_of(Identity))
+  #Metadata <- Metadata %>% rename(specimen = .data[[Identity]])
+  #Metadata <- Metadata %>% unique()
   }
   
-  Metadata <- Metadata %>% rename(specimen = all_of(Identity))
-  #Metadata <- Metadata %>% rename(specimen = .data[[Identity]])
-  Metadata <- Metadata %>% unique() # Handling Issues With Repeated Rows.
 
+  # Assembling rowNames
 
+  Filtered <- x
+  TheWeightedDataset <- Filtered %>% group_by(Clusters) %>%
+    summarise(Total_Counts = sum(Count)) %>% arrange((Total_Counts))
+  TheDataset <- TheWeightedDataset %>% rename(Identity = Clusters) %>%
+    arrange(desc(Total_Counts)) %>% select(Identity)
+  TheDataset$Identity <- gsub("pos", "pos_", TheDataset$Identity)
+  TheDataset$Identity <- gsub("neg", "neg-", TheDataset$Identity)
+  Upload <- TheDataset
 
+  #strsplit divides by character. unnest then changes downward into rows.
+  A <- Upload %>% mutate(to = strsplit(Identity, "_")) %>% unnest(to)
+  B <- A %>% mutate(to = strsplit(to, "-")) %>% unnest(to)
+  B$to <- gsub("pos", "_pos", gsub("neg", "_neg", B$to))
+
+  #B <- B %>% filter(!to %in% c("NABV605_neg", "NABV711_pos", "NAZombieNIR_neg"))
+
+  C <- B %>% separate_wider_delim(to, delim = "_", names = c("Fluorophore", "Value")) %>%
+    pivot_wider(names_from = Fluorophore, values_from = Value)
+  C[C == "pos"] <- "1"
+  C[C == "neg"] <- "0"
+  ColsC <- ncol(C)
+  C[,2:ColsC] <- lapply(C[,2:ColsC], as.numeric)
+  return(C)
 
 
 }
